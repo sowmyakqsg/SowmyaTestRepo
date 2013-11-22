3666d60e029a010bfa7359b6cf456fbbe1523678
sowmya <sowmyak@qsgsoft.com>
2013-11-22 11:47:00 +0530

    Sowmya - Committed initial project

---
+++ selenium-rc-test/src/test/java/functionality/SeleniumTest.java
@@ -0,0 +1,14 @@
+package functionality;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
