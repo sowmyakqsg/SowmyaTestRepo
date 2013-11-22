2ebe096b4bf4e0983d3bd27438091b5de8e6b1c5
sowmya <sowmyak@qsgsoft.com>
2013-11-22 12:02:27 +0530

    Sowmya - Committed initial webdriver project

---
+++ selenium-webdriver-test/src/test/java/functionality/SeleniumDriverTest.java
@@ -0,0 +1,14 @@
+package functionality;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SeleniumDriverTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
