ced1b767f4dcffffdc8c9a61a9f96ead8b8df494
sowmya <sowmyak@qsgsoft.com>
2013-11-29 11:10:57 +0530

    Sowmya - Committed selenium web driver backed scripts

--- selenium-rc-test/src/test/java/functionality/SeleniumTest.java
+++ selenium-rc-test/src/test/java/functionality/SeleniumTest.java
@@ -1,14 +1,23 @@
 package functionality;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.Selenium;
 
 public class SeleniumTest {
+	Selenium selenium;
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		selenium = new DefaultSelenium("localhost",4444,"*firefox","http://iaautoqa63.dyndns.org/exemplar/exemplar/Portal.action");
+		selenium.start();
+		selenium.open("http://iaautoqa63.dyndns.org/exemplar/exemplar/Portal.action");
+		selenium.windowMaximize();
+		selenium.click("//a[@id='rLogin']");
+		selenium.waitForPageToLoad("120000");
+		System.out.println(selenium.getText("//div[6]/label"));
+		//System.out.println(selenium.getText("//td[@align='left']//a[2]"));
+		selenium.close();
+		selenium.stop();
 	}
-
 }
